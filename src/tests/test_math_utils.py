6d96e9dd8067510dac82f1b819d55de54135d41c
benjkahn <ben.kahn.2012@gmail.com>
2025-11-17 16:58:19 -0800

    working unit test for quat mult

--- src/tests/test_math_utils.py
+++ src/tests/test_math_utils.py
@@ -1,15 +1,14 @@
-import unittest as test
+import pytest as test
 import numpy as np
-from models import quat_utils
+from src.models import quat_utils
 
-class Test_Quaternion_Mult(test.TestCase):
     
-    def test_quaternion_multiply_random():
-        q1 = np.array(2,1,0,0)
-        q2 = np.array(3,0,4,0)
+def test_quaternion_multiply_random():
+    q1 = np.array([2,1,0,0])
+    q2 = np.array([3,0,4,0])
 
-        actual_output = quat_utils.quaternion_multiplication(q1,q2)
-        expected_output = np.array(6,3,8,4)
+    actual_output = quat_utils.quaternion_multiplication(q1,q2)
+    expected_output = np.array([6,3,8,4])
 
-        test.assertEqual(actual_output,expected_output)
+    assert actual_output.all() == expected_output.all()
 
