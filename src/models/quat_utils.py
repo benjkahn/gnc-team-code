ea899517f8e25459b126842e785955975a2037d3
benjkahn <ben.kahn.2012@gmail.com>
2025-11-17 17:02:46 -0800

    intentionally wrong

--- src/models/quat_utils.py
+++ src/models/quat_utils.py
@@ -28,7 +28,7 @@ def quaternion_multiplication(q1: np.ndarray, q2: np.ndarray):
     w1, x1, y1, z1 = q1
     w2, x2, y2, z2 = q2
 
-    w = w1*w2 - x1*x2 - y1*y2 - z1*z2
+    w = w1*w2 + x1*x2 - y1*y2 - z1*z2
     x = w1*x2 + x1*w2 + y1*z2 - z1*y2
     y = w1*y2 - x1*z2 + y1*w2 + z1*x2
     z = w1*z2 + x1*y2 - y1*x2 + z1*w2
